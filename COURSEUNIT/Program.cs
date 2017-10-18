c2e2af74c1616a1e033ebd643dce4c2fc2f15564
Dima <Dima>
2017-10-18 18:00:25 +0300

    get fields, methods? propertys

--- COURSEUNIT/Program.cs
+++ COURSEUNIT/Program.cs
@@ -12,13 +12,41 @@ namespace Course
         {
         }
     }
-    public class A
+    public class A : ICar
     {
+        public string publicValue = "public";
+        string privateValue = "private";
+
+        public int PublicAutoProperty{ get; set;}
+        int PrivateAutoProperty { get; set; }
+
+        public string PublicProperty
+        {
+            get { return publicValue; }
+            set { publicValue = value; }
+        }
+
+        string PrivateProperty
+        {
+            get { return privateValue; }
+        }
+        
         private double Div(int a, int b)
         {
             //return a / b; 
             return (double)a / (b + a);
         }
+
+        public void Drive() { }
+        void ICar.Access() { }
+    }
+
+
+
+    interface ICar
+    {
+        void Drive();
+        void Access();
     }
 
 }
--- UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTest1.cs
@@ -9,6 +9,69 @@ namespace UnitTestProject1
     [TestClass]
     public class UnitTest1
     {
+        A a = new A();
+
+        [TestMethod]
+        public void various_ways_to_get_type()
+        {
+            //var a = new A();
+            Type[] arr_type = new Type[3];
+            arr_type[0] = typeof(A);
+            arr_type[1] = a.GetType();
+            arr_type[2] = Type.GetType("UnitTestProject1.UnitTest1");
+            foreach (Type item in arr_type)
+            {
+                Console.WriteLine("Type: {0}, Namespace: {1}", item.Name, item.Namespace);
+            } 
+
+        }
+
+        [TestMethod]
+        public void get_fields()
+        {
+            Type aType = a.GetType();
+            FieldInfo[] arrField = aType.GetFields(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            foreach (FieldInfo item in arrField)
+            {
+                Console.WriteLine("Field: {0}||, " +
+                    "Field Type1: {1}||, " +
+                    "Field Type2: {2}||", item.Name, item.DeclaringType, item.FieldType);
+            }
+        }
+
+        [TestMethod]
+        public void get_property()
+        {
+            Type aType = a.GetType();
+            PropertyInfo[] arrProp = aType.GetProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            foreach (PropertyInfo item in arrProp)
+            {
+                Console.WriteLine("Property name: {0}||, " +
+                    "Property Type1: {1}||, " +
+                    "Property Type2: {2}||", item.Name, item.DeclaringType, item.PropertyType );
+            }
+
+        }
+
+        [TestMethod]
+        public void get_methods()
+        {
+            Type aType = a.GetType();
+            MethodInfo[] arrMethod = aType.GetMethods(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+            foreach (MethodInfo item in arrMethod)
+            {
+                Console.WriteLine("Method name: {0}, ", item.Name);
+            }
+
+            Console.WriteLine(new string('-', 20));
+
+            arrMethod = aType.GetMethods(BindingFlags.DeclaredOnly | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static);
+            foreach (MethodInfo item in arrMethod)
+            {
+                Console.WriteLine("Method name: {0}, ", item.Name);
+            }
+        }
+
         [TestMethod]
         public void Divizion_on_b_correcrt_value()
         {
@@ -22,7 +85,7 @@ namespace UnitTestProject1
             //var actualResult = divMethod.Invoke(a, new object [] {1,1} );
 
             //2-й способ рефлексии
-            var divMethod = atype.GetMethod("Div", BindingFlags.NonPublic | BindingFlags.Instance);
+            MethodInfo divMethod = atype.GetMethod("Div", BindingFlags.NonPublic | BindingFlags.Instance);
             var actualResult = divMethod.Invoke(a, new object[] { 1, 1 });
 
 
